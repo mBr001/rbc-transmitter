03fac8a616253620056089be0d8dd1293198d69d
stsdc <s.dac@yandex.ru>
2017-11-29 18:39:09 +0100

    works randomly

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <RFM69.h>
 #include <SPI.h>
+// #include <list>
 //#include <SPIFlash.h>
 
 #define NODEID 99
@@ -8,7 +9,7 @@
 #define FREQUENCY RF69_868MHZ  //RF69_433MHZ //Match this with the version of your Moteino! (others: RF69_433MHZ, RF69_868MHZ)
 #define KEY "sampleEncryptKey" //has to be same 16 characters/bytes on all nodes, not more not less!
 #define LED 9
-#define SERIAL_BAUD 19200
+#define BAUDRATE 57600
 #define ACK_TIME 50 // # of ms to wait for an ack
 //#define IS_RFM69HW_HCW  //uncomment only for RFM69HW/HCW! Leave out if you have RFM69W/CW!
 
@@ -19,32 +20,57 @@ boolean requestACK = false;
 //SPIFlash flash(8, 0xEF30); //EF40 for 16mbit windbond chip
 RFM69 radio;
 
-typedef struct
-{
+typedef struct {
     uint16_t nodeId; //store this nodeId
-    int v0;          //erature maybe?
-    int v1;
+    // unsigned long long int command; // oh yeah...
+    int data[255];
 } Payload;
-Payload theData;
+Payload payload;
 
-String inputString = "";
+int received[255];
+
 boolean stringComplete = false;
 byte input = 0;
 char tmp[6];
 int adres = 20;
 
-void Blink(byte PIN, int DELAY_MS)
-{
-    pinMode(PIN, OUTPUT);
-    digitalWrite(PIN, HIGH);
-    delay(DELAY_MS);
-    digitalWrite(PIN, LOW);
+void blink(byte pin, int delay_ms) {
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, HIGH);
+    delay(delay_ms);
+    digitalWrite(pin, LOW);
 }
 
+void pong() {
+    Serial.print('[');
+    Serial.print(radio.SENDERID, DEC);
+    Serial.print("] ");
+    for (byte i = 0; i < radio.DATALEN; i++)
+        Serial.print((char)radio.DATA[i]);
+    Serial.print("   [RX_RSSI:");
+    Serial.print(radio.readRSSI());
+    Serial.print("]");
 
-void setup()
-{
-    Serial.begin(SERIAL_BAUD);
+    if (radio.ACKRequested())
+    {
+        radio.sendACK();
+        Serial.print(" - ACK sent");
+        delay(10);
+    }
+    blink(LED, 5);
+    Serial.println();
+}
+
+void clean_payload_data(){
+    int data_length = sizeof(payload.data) / sizeof(payload.data[0]);
+    for (int i = 0; i < data_length; i++)
+    {
+        payload.data[i] = null;
+    }
+}
+
+void setup(){
+    Serial.begin(BAUDRATE);
     radio.initialize(FREQUENCY, NODEID, NETWORKID);
     //radio.setHighPower(); //uncomment only for RFM69HW!
     radio.encrypt(KEY);
@@ -53,111 +79,57 @@ void setup()
     Serial.println(buff);
 }
 
+void send() {
+    Serial.print("Sending struct (");
+    Serial.print(sizeof(payload));
+    Serial.print(" bytes) ... ");
+    if (radio.sendWithRetry(GATEWAYID, (const void *)(&payload), sizeof(payload)))
+        Serial.print(" ok!\n");
+    else
+        Serial.print(" nothing...\n");
+    blink(LED, 3);
+}
+
 long lastPeriod = -1;
-void loop()
-{
-    if (stringComplete)
-    {
-        Serial.println(inputString);
-
-        if (inputString[0] == 'F')
-        {
-            for (int i = 0; i < 4; i++)
-            {
-                tmp[i] = inputString[i + 6];
+void loop() {
+    if (stringComplete) {
+        // accept only our glorious protocol
+        if (received[0] == 0xFF) {
+            Serial.print("ACCEPTED: ");
+            for (int i = 0; i < 255; i++){
+                payload.data[i] = received[i];
+                Serial.print(payload.data[i]);
             }
-            Serial.println(tmp);
-            theData.v0 = atoi(tmp);
-            for (int i = 0; i < 4; i++)
-            {
-                tmp[i] = inputString[i + 10];
-            }
-            Serial.println(tmp);
-            theData.v1 = atoi(tmp);
-
-            Serial.print(theData.v0);
-            Serial.print(" ");
-            Serial.println(theData.v1);
+            Serial.print("\n");
+        } else {
+            Serial.print("NONONO\n");
         }
-        if (inputString[0] == '1')
-        {
-            TRANSMITPERIOD = 50 * (1);
-            if (TRANSMITPERIOD == 0)
-                TRANSMITPERIOD = 1000;
-            Serial.print("\nChanging delay to ");
-            Serial.print(TRANSMITPERIOD);
-            Serial.println("ms\n");
-        }
-        if (inputString[0] == '2')
-        {
-            TRANSMITPERIOD = 50 * (2);
-            if (TRANSMITPERIOD == 0)
-                TRANSMITPERIOD = 1000;
-            Serial.print("\nChanging delay to ");
-            Serial.print(TRANSMITPERIOD);
-            Serial.println("ms\n");
-        }
-
-        inputString = "";
         stringComplete = false;
-    } //serial
+    }
 
     //check for any received packets
-    if (radio.receiveDone())
-    {
-        Serial.print('[');
-        Serial.print(radio.SENDERID, DEC);
-        Serial.print("] ");
-        for (byte i = 0; i < radio.DATALEN; i++)
-            Serial.print((char)radio.DATA[i]);
-        Serial.print("   [RX_RSSI:");
-        Serial.print(radio.readRSSI());
-        Serial.print("]");
-
-        if (radio.ACKRequested())
-        {
-            radio.sendACK();
-            Serial.print(" - ACK sent");
-            delay(10);
-        }
-        Blink(LED, 5);
-        Serial.println();
+    if (radio.receiveDone()) {
+        // pong();
     }
 
     int currPeriod = millis() / TRANSMITPERIOD;
-    if (currPeriod != lastPeriod)
-    {
-        //fill in the struct with new values
-        theData.nodeId = 0x20;
-        //theData.uptime = millis();
-
-        Serial.print("Sending struct (");
-        Serial.print(sizeof(theData));
-        Serial.print(" bytes) ... ");
-        if (radio.sendWithRetry(GATEWAYID, (const void *)(&theData), sizeof(theData)))
-            Serial.print(" ok!");
-        else
-            Serial.print(" nothing...");
-        Serial.println();
-        Blink(LED, 3);
+    if (currPeriod != lastPeriod) {
+        send();
         lastPeriod = currPeriod;
+        clean_payload_data();
     }
 
 } //loop
 
-void serialEvent()
-{
-    while (Serial.available())
-    {
-        // get the new byte:
-        char inChar = (char)Serial.read();
-        // add it to the inputString:
-        inputString += inChar;
-        // if the incoming character is a newline, set a flag so the main loop can
-        // do something about it:
-        if (inChar == '\n')
-        {
+void serialEvent() {
+    int i = 0;
+    while (Serial.available()) {
+        received[i] = Serial.read();
+        // Serial.print(received[i],HEX);
+        if (received[i] == 0x0A){
             stringComplete = true;
         }
+        i++;
     }
 }
+
