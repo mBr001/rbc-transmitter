08d4c565d44f6010ba669a8346e31465be016142
stsdc <s.dac@yandex.ru>
2017-11-30 21:34:46 +0100

    mostly works

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
+// #include <ArduinoSTL.h>
 #include <RFM69.h>
 #include <SPI.h>
-// #include <list>
+#include <vector>
 //#include <SPIFlash.h>
+using namespace std;
 
 // disables or enables printing to serial
 #define print(var) Serial.print(var)
@@ -28,11 +30,11 @@ boolean requestACK = false;
 RFM69 radio;
 
 typedef struct {
-  byte data[255];
+  vector<byte> data;
 } Payload;
 Payload payload;
 
-byte received[255];
+vector<byte> received;
 boolean is_complete = false;
 
 void blink(byte pin, int delay_ms) {
@@ -60,20 +62,13 @@ void pong() {
   blink(LED, 5);
 }
 
-void clean_payload_data() {
-  int data_length = sizeof(payload.data) / sizeof(payload.data[0]);
-  for (int i = 0; i < data_length; i++) {
-    payload.data[i] = null;
-  }
-}
-
 void setup() {
   Serial.begin(BAUDRATE);
   radio.initialize(FREQUENCY, NODEID, NETWORKID);
   // radio.setHighPower(); //uncomment only for RFM69HW!
   radio.encrypt(KEY);
   char buff[50];
-  sprintf(buff, "\nTransmitting at %d Mhz...",
+  sprintf(buff, "\nTransmitting at %d Mhz...\n",
           FREQUENCY == RF69_433MHZ ? 433
                                    : FREQUENCY == RF69_868MHZ ? 868 : 915);
   print(buff);
@@ -94,39 +89,36 @@ long lastPeriod = -1;
 void loop() {
   if (is_complete) {
     // accept only our glorious protocol
-    if (received[0] == 0xFF) {
-      print("ACCEPTED: ");
-      for (int i = 0; i < 255; i++) {
-        payload.data[i] = received[i];
-        print(payload.data[i]);
-      }
-      print("\n");
+    if (received.front() == 0xFF) {
+      print("ACCEPTED\n");
+      payload.data = received;
     } else {
       print("REJECTED\n");
     }
     is_complete = false;
+    received.clear();
   }
 
   // check for any received packets
   if (radio.receiveDone()) {
-    pong();
+    // pong();
   }
 
   int currPeriod = millis() / TRANSMITPERIOD;
   if (currPeriod != lastPeriod) {
     send();
     lastPeriod = currPeriod;
-    clean_payload_data();
   }
 
-} // loop
+}
 
 void serialEvent() {
   int i = 0;
   while (Serial.available()) {
-    received[i] = Serial.read();
-    // Serial.println(received[i], HEX);
-    if (received[i] == 0x0A) {
+    byte incoming_byte = Serial.read();
+    received.push_back(incoming_byte);
+    // Serial.println(received.front(), HEX);
+    if (incoming_byte == 0x0A) {
       is_complete = true;
     }
     i++;
