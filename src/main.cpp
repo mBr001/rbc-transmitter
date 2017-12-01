2e2d6cf9b2607ab0ca601785fd3d80d1ae0f22eb
stsdc <s.dac@yandex.ru>
2017-12-01 01:52:02 +0100

    used array instead of struct

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
-// #include <ArduinoSTL.h>
 #include <RFM69.h>
 #include <SPI.h>
 #include <vector>
 //#include <SPIFlash.h>
 using namespace std;
-
 // disables or enables printing to serial
 #define print(var) Serial.print(var)
 // #define print(var)
@@ -25,16 +23,15 @@ using namespace std;
 // have RFM69W/CW!
 
 int TRANSMITPERIOD = 200; // transmit a packet to gateway so often (in ms)
+int licz = 0;
+byte sendSize = 0;
 boolean requestACK = false;
 // SPIFlash flash(8, 0xEF30); //EF40 for 16mbit windbond chip
 RFM69 radio;
 
-typedef struct {
-  vector<byte> data;
-} Payload;
-Payload payload;
-
+byte data[255];
 vector<byte> received;
+
 boolean is_complete_frame = false;
 
 void blink(byte pin, int delay_ms) {
@@ -46,7 +43,7 @@ void blink(byte pin, int delay_ms) {
 
 void pong() {
   print('[');
-  print(radio.SENDERID);
+  // Sprintln(radio.SENDERID, DEC);
   print("] ");
   for (byte i = 0; i < radio.DATALEN; i++)
     print((char)radio.DATA[i]);
@@ -60,6 +57,7 @@ void pong() {
     delay(10);
   }
   blink(LED, 5);
+  // println();
 }
 
 void setup() {
@@ -68,15 +66,15 @@ void setup() {
   // radio.setHighPower(); //uncomment only for RFM69HW!
   radio.encrypt(KEY);
   char buff[50];
-  sprintf(buff, "\nTransmitting at %d Mhz...\n", FREQUENCY == RF69_433MHZ ? 433 : FREQUENCY == RF69_868MHZ ? 868 : 915);
+  sprintf(buff, "\nTransmitting at %d Mhz...", FREQUENCY == RF69_433MHZ ? 433 : FREQUENCY == RF69_868MHZ ? 868 : 915);
   print(buff);
 }
 
 void send() {
   print("Sending struct (");
-  print(sizeof(payload));
+  print(sizeof(data));
   print(" bytes) ... ");
-  if (radio.sendWithRetry(GATEWAYID, (const void *)(&payload), sizeof(payload))) {
+  if (radio.sendWithRetry(GATEWAYID, (const void *)(&data), sizeof(data))) {
     print(" ok!\n");
   } else {
     print(" nothing...\n");
@@ -89,8 +87,11 @@ void loop() {
   if (is_complete_frame) {
     // accept only our glorious protocol
     if (received.front() == 0xFF) {
-      print("ACCEPTED\n");
-      payload.data = received;
+      print("ACCEPTED: ");
+      copy(received.begin(), received.end(), data);
+      // std::vector<double> v;
+      // double *a = &v[0];
+      print("\n");
     } else {
       print("REJECTED\n");
     }
@@ -98,7 +99,6 @@ void loop() {
     received.clear();
   }
 
-  // check for any received packets
   if (radio.receiveDone()) {
     // pong();
   }
@@ -114,7 +114,7 @@ void serialEvent() {
   while (Serial.available()) {
     byte incoming_byte = Serial.read();
     received.push_back(incoming_byte);
-    // Serial.println(received.front(), HEX);
+    // Serial.println(received[i], HEX);
     if (incoming_byte == 0x0A) {
       is_complete_frame = true;
     }
